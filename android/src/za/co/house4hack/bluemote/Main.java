defda42f057d0f7d73efb9983f5ee9ae61f1ac4b
toby <toby@tobykurien>
2012-05-03 15:09:44 +0200

    wip to make make the service

--- android/src/za/co/house4hack/bluemote/Main.java
+++ android/src/za/co/house4hack/bluemote/Main.java
@@ -11,11 +11,13 @@ import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.AssetFileDescriptor;
@@ -25,6 +27,7 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
@@ -33,7 +36,7 @@ import android.widget.Toast;
 public class Main extends Activity {
    private static final String SHAC_OPEN = "za.co.house4hack.shac.OPEN";
 
-private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
+   private static final String BLUEMOTE_NAME_PREFIX = "BluMote";
 
    // Debugging
    private static final String TAG = "BlueMote";
@@ -60,11 +63,40 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
    // Array adapter for the conversation thread
    private BluetoothAdapter mBluetoothAdapter = null;
    // Member object for the chat services
-   private BluetoothService mService = null;
    private BluetoothAdapter mBtAdapter;
 
    private ProgressDialog pd = null;
    private boolean pairing = false;
+
+   private BluetoothService mService = null;
+   public static boolean mIsBound;
+   private ServiceConnection mConnection = new ServiceConnection() {
+      public void onServiceConnected(ComponentName className, IBinder service) {
+         BluetoothService.LocalBinder binder = (BluetoothService.LocalBinder) service;
+         mService = binder.getService();
+      }
+
+      public void onServiceDisconnected(ComponentName className) {
+         mService = null;
+      }
+   };
+
+   void doBindService() {
+      // Establish a connection with the service. We use an explicit
+      // class name because we want a specific service implementation that
+      // we know will be running in our own process (and thus won't be
+      // supporting component replacement by other applications).
+      bindService(new Intent(Main.this, BluetoothService.class), mConnection, Context.BIND_AUTO_CREATE);
+      mIsBound = true;
+   }
+
+   void doUnbindService() {
+      if (mIsBound) {
+         // Detach our existing connection.
+         unbindService(mConnection);
+         mIsBound = false;
+      }
+   }
 
    /** Called when the activity is first created. */
    @Override
@@ -82,6 +114,7 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
          return;
       }
 
+      doBindService();
    }
 
    @Override
@@ -99,32 +132,37 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
          if (mService == null) setupChat();
       }
    }
-
-   @Override
-   protected void onResume() {
-      // TODO Auto-generated method stub
-      super.onResume();
-
-      // Performing this check in onResume() covers the case in which BT was
-      // not enabled during onStart(), so we were paused to enable it...
-      // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-      if (mService != null) {
-         // Only if the state is STATE_NONE, do we know that we haven't started
-         // already
-         if (mService.getState() == BluetoothService.STATE_NONE) {
-            // Start the Bluetooth chat services
-            mService.start();
-         }
-      }
-   }
+//
+//   @Override
+//   protected void onResume() {
+//      // TODO Auto-generated method stub
+//      super.onResume();
+//
+//      // Performing this check in onResume() covers the case in which BT was
+//      // not enabled during onStart(), so we were paused to enable it...
+//      // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+//      if (mService != null) {
+//         // Only if the state is STATE_NONE, do we know that we haven't started
+//         // already
+//         if (mService.getState() == BluetoothService.STATE_NONE) {
+//            // Start the Bluetooth chat services
+//            mService.start();
+//         }
+//      }
+//   }
 
    @Override
    protected void onDestroy() {
       // TODO Auto-generated method stub
       super.onDestroy();
       // Stop the Bluetooth chat services
-      if (mService != null) mService.stop();
-      try { unregisterReceiver(mReceiver); } catch (Exception e) {}
+      // if (mService != null) mService.stop();
+      doUnbindService();
+      
+      try {
+         unregisterReceiver(mReceiver);
+      } catch (Exception e) {
+      }
    }
 
    public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -158,7 +196,7 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
 
    public void setupChat() {
       // Initialize the BluetoothService to perform bluetooth connections
-      mService = new BluetoothService(this, mHandler);
+//      mService = new BluetoothService(this, mHandler);
    }
 
    public void connectDevice(final BluetoothDevice device, final boolean secure) {
@@ -175,40 +213,38 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
     * @param v
     */
    public void onConnect(View v) {
-      new AlertDialog.Builder(this).setTitle("Connect BlueMotes")
-      .setMessage("Please press button 1 and 2 on your BlueMotes to pair them")
-      .setPositiveButton("Ready", new OnClickListener() {
+      new AlertDialog.Builder(this).setTitle("Connect BlueMotes").setMessage("Please press button 1 and 2 on your BlueMotes to pair them")
+               .setPositiveButton("Ready", new OnClickListener() {
 
-         @Override
-         public void onClick(DialogInterface dialog, int which) {
-            dialog.dismiss();
-            pairing = true;
-            
-            // Register for broadcasts when a device is discovered
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mReceiver, filter);
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                     dialog.dismiss();
+                     pairing = true;
 
-            // Register for broadcasts when discovery has finished
-            filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-            registerReceiver(mReceiver, filter);
+                     // Register for broadcasts when a device is discovered
+                     IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                     registerReceiver(mReceiver, filter);
 
-            // Get the local Bluetooth adapter
-            mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+                     // Register for broadcasts when discovery has finished
+                     filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+                     registerReceiver(mReceiver, filter);
 
-            // Get a set of currently paired devices
-            Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
-            for (BluetoothDevice d : pairedDevices) {
-               if (d.getName().startsWith(BLUEMOTE_NAME_PREFIX)) {
-                  // found our device, connect to it
-                  Log.d(TAG, "Found device " + d.getName() + " - " + d.getAddress());
-                  connectDevice(d, true);
-               }
-            }
+                     // Get the local Bluetooth adapter
+                     mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
-            doDiscovery();
-         }
-      })
-      .create().show();
+                     // Get a set of currently paired devices
+                     Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+                     for (BluetoothDevice d : pairedDevices) {
+                        if (d.getName().startsWith(BLUEMOTE_NAME_PREFIX)) {
+                           // found our device, connect to it
+                           Log.d(TAG, "Found device " + d.getName() + " - " + d.getAddress());
+                           connectDevice(d, true);
+                        }
+                     }
+
+                     doDiscovery();
+                  }
+               }).create().show();
    }
 
    public void onDisconnect(View v) {
@@ -243,19 +279,19 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
                if (D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
                switch (msg.arg1) {
                   case BluetoothService.STATE_CONNECTED:
-                     Toast.makeText(getApplicationContext(),
-                              "Connected " + msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
-                              .show();
+                     Toast.makeText(getApplicationContext(), "Connected " + msg.getData().getString(TOAST), Toast.LENGTH_SHORT).show();
                      if (pairing) sendBluetooth("*");
                      break;
                   case BluetoothService.STATE_CONNECTING:
-//                     Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
-//                              Toast.LENGTH_SHORT).show();
+                     // Toast.makeText(getApplicationContext(),
+                     // msg.getData().getString(TOAST),
+                     // Toast.LENGTH_SHORT).show();
                      break;
                   case BluetoothService.STATE_LISTEN:
                   case BluetoothService.STATE_NONE:
-//                     Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
-//                              Toast.LENGTH_SHORT).show();
+                     // Toast.makeText(getApplicationContext(),
+                     // msg.getData().getString(TOAST),
+                     // Toast.LENGTH_SHORT).show();
                      break;
                }
                break;
@@ -269,21 +305,18 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
                byte[] readBuf = (byte[]) msg.obj;
                // construct a string from the valid bytes in the buffer
                String readMessage = new String(readBuf, 0, msg.arg1);
-               if (D) Toast.makeText(getApplicationContext(), mConnectedDeviceName + ":  " + readMessage,
-                        Toast.LENGTH_SHORT).show();
+               if (D) Toast.makeText(getApplicationContext(), mConnectedDeviceName + ":  " + readMessage, Toast.LENGTH_SHORT).show();
                processCommand(new String(readBuf));
                break;
             case MESSAGE_DEVICE_NAME:
                // save the connected device's name
                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
                if (D && mConnectedDeviceName != null) {
-                  Toast.makeText(getApplicationContext(), "Connected to " + mConnectedDeviceName,
-                           Toast.LENGTH_SHORT).show();
+                  Toast.makeText(getApplicationContext(), "Connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
                }
                break;
             case MESSAGE_TOAST:
-               Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
-                        Toast.LENGTH_SHORT).show();
+               Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST), Toast.LENGTH_SHORT).show();
                break;
          }
       }
@@ -321,8 +354,7 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
             // If it's already paired, skip it, because it's been listed already
             if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
                if (device.getName().startsWith(BLUEMOTE_NAME_PREFIX)) {
-                  Log.d(TAG,
-                           "Found unpaired device " + device.getName() + " - " + device.getAddress());
+                  Log.d(TAG, "Found unpaired device " + device.getName() + " - " + device.getAddress());
                   connectDevice(device, false);
                }
             }
@@ -338,53 +370,50 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
    protected void processCommand(String cmd) {
       // for now we support 4 buttons
       if (cmd.startsWith("1")) {
-         /*Intent intent = new Intent(Intent.ACTION_CALL);
-         intent.setData(Uri.parse("tel:10111"));
-         startActivity(intent);*/
-         if(isIntentAvailable(this,SHAC_OPEN)){    	  
-	    	  Intent intent = new Intent(SHAC_OPEN);
-	    	  intent.putExtra("access", "door");
-	    	  startActivity(intent);
+         /*
+          * Intent intent = new Intent(Intent.ACTION_CALL);
+          * intent.setData(Uri.parse("tel:10111"));
+          * startActivity(intent);
+          */
+         if (isIntentAvailable(this, SHAC_OPEN)) {
+            Intent intent = new Intent(SHAC_OPEN);
+            intent.putExtra("access", "door");
+            startActivity(intent);
          } else {
-        	 Toast.makeText(this, "SHAC is not installed", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "SHAC is not installed", Toast.LENGTH_SHORT).show();
          }
       } else if (cmd.startsWith("2")) {
-         soundAlarm(this);         
+         soundAlarm(this);
       } else if (cmd.startsWith("3")) {
          recordAudio(this);
       } else if (cmd.startsWith("4")) {
-         
+
       }
    }
 
    public boolean isIntentAvailable(Context context, String action) {
-	    final PackageManager packageManager = context.getPackageManager();
-	    final Intent intent = new Intent(action);
-	    List<ResolveInfo> resolveInfo =
-	            packageManager.queryIntentActivities(intent,
-	                    PackageManager.MATCH_DEFAULT_ONLY);
-	   if (resolveInfo.size() > 0) {
-	   		return true;
-	   	}
-	   return false;
-	}
-   
-   
+      final PackageManager packageManager = context.getPackageManager();
+      final Intent intent = new Intent(action);
+      List<ResolveInfo> resolveInfo = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+      if (resolveInfo.size() > 0) { return true; }
+      return false;
+   }
+
    private void recordAudio(Context context) {
       final MediaRecorder recorder = new MediaRecorder();
       try {
          recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
          recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
          recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-         recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + 
-                  "/BluMoteRecord" + new Date().getTime() + ".3gp");
+         recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/BluMoteRecord" + new Date().getTime()
+                  + ".3gp");
          recorder.prepare();
       } catch (IOException e1) {
          Toast.makeText(context, "Error recording" + e1.getMessage(), Toast.LENGTH_LONG).show();
       }
-      
-      recorder.start();   // Recording is now started
-      
+
+      recorder.start(); // Recording is now started
+
       Thread t = new Thread() {
          public void run() {
             try {
@@ -392,13 +421,14 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
             } catch (InterruptedException e) {
             }
             recorder.stop();
-            recorder.reset();   // You can reuse the object by going back to setAudioSource() step
-            recorder.release(); // Now the object cannot be reused      
+            recorder.reset(); // You can reuse the object by going back to
+                              // setAudioSource() step
+            recorder.release(); // Now the object cannot be reused
          };
       };
       t.start();
    }
-   
+
    /**
     * Sound the anti-theft alarm
     * 
@@ -421,7 +451,7 @@ private static final String BLUEMOTE_NAME_PREFIX = "BlueMote";
          player.prepare();
          player.setLooping(true);
          player.start();
-         
+
          Thread t = new Thread() {
             public void run() {
                try {
